c012d4815ea352acef39284a4c571793ce2bccfd
Lucas Keller <lucaskeller98@gmail.com>
2018-07-30 18:32:38 +0200

    finished register design

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -7,25 +7,4 @@ import javafx.fxml.FXML;
 
 public class Controller {
 
-    @FXML
-    private JFXTextField emailField;
-    @FXML
-    private JFXTextField nameField;
-    @FXML
-    private JFXPasswordField passwordField;
-
-
-    public void initialize(){
-        passwordField.setLabelFloat(true);
-        emailField.setPromptText("Enter an Email");
-        nameField.setPromptText("Enter a Nickname");
-        passwordField.setPromptText("Enter a Password");
-
-        RequiredFieldValidator validator = new RequiredFieldValidator();
-        nameField.getValidators().add(validator);
-        validator.setMessage("Input Required");
-        nameField.focusedProperty().addListener((o,oldVal,newVal)-> {
-            if (!newVal) nameField.validate();
-        });
-    }
 }
--- src/sample/windows/RegisterController.java
+++ src/sample/windows/RegisterController.java
@@ -1,5 +1,48 @@
 package sample.windows;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.validation.RequiredFieldValidator;
+import javafx.fxml.FXML;
+
 public class RegisterController {
 
+    @FXML
+    private JFXTextField emailField;
+    @FXML
+    private JFXTextField nameField;
+    @FXML
+    private JFXPasswordField passwordField;
+    @FXML
+    private JFXPasswordField confirmationlField;
+    @FXML
+    private JFXButton submitButton;
+
+
+    public void initialize() {
+
+        RequiredFieldValidator validator = new RequiredFieldValidator();
+        nameField.getValidators().add(validator);
+        emailField.getValidators().add(validator);
+        passwordField.getValidators().add(validator);
+        confirmationlField.getValidators().add(validator);
+        validator.setMessage("Input Required");
+        nameField.focusedProperty().addListener((o, oldVal, newVal) -> {
+            if (!newVal) nameField.validate();
+        });
+        passwordField.focusedProperty().addListener((o, oldVal, newVal) -> {
+            if (!newVal) passwordField.validate();
+        });
+        confirmationlField.focusedProperty().addListener((o, oldVal, newVal) -> {
+            if (!newVal) confirmationlField.validate();
+        });
+        emailField.focusedProperty().addListener((o, oldVal, newVal) -> {
+            if (!newVal) emailField.validate();
+        });
+
+
+
+
+    }
 }
