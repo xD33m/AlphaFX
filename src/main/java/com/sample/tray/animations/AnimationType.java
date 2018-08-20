6a9bf02d8f1b47f53047fd65ad4bfec5441f9c51
Lucas Keller <lucaskeller98@gmail.com>
2018-08-20 10:53:07 +0200

    added windows notification

---
+++ src/main/java/com/sample/tray/animations/AnimationType.java
@@ -0,0 +1,7 @@
+package com.sample.tray.animations;
+
+public enum AnimationType {
+    FADE,
+    SLIDE,
+    POPUP
+}
---
+++ src/main/java/com/sample/tray/models/Location.java
@@ -0,0 +1,20 @@
+package com.sample.tray.models;
+
+
+public class Location {
+
+    private double x, y;
+
+    public Location(double xLoc, double yLoc) {
+        this.x = xLoc;
+        this.y = yLoc;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+}
